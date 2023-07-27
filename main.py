920203ce72d9f23975203b3d10e745d128a00e0b
doneni <xxerrenn@icloud.com>
2023-07-27 21:24:07 +0900

    Update DockerFile

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
         uvicorn.run(
             "main:app",
             host='0.0.0.0',
-            port=8080,
+            port=80,
             workers=5,
             log_level='warning',
             reload=False,
